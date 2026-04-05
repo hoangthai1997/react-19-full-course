d8ee16511269136d0a13e04bd21b6b586eec6fa2
Vương Quốc Hoàng Thái <vuongquochoangthai@gmail.com>
2026-04-05 19:05:50 +0700

    Form

---
+++ src/components/AddTodoForm.tsx
@@ -0,0 +1,26 @@
+import { useState } from "react";
+import type { FormEvent } from "react";
+
+interface AddTodoFormProps {
+    onSubmit: (title: string) => void;
+}
+
+export default function AddTodoForm({onSubmit}: AddTodoFormProps) {
+    const [input, setInput] = useState("");
+
+    function handleSubmit(e: FormEvent<HTMLFormElement>) {
+        e.preventDefault();
+
+        if (!input.trim()) return;
+
+        onSubmit(input);
+        setInput("");
+    }
+
+    return (
+        <form className="flex" onSubmit={handleSubmit}>
+            <input type="text" value={input} onChange={(e) => setInput(e.target.value)} placeholder="What needs to be done?" className="rounded-s-md grow border border-gray-400 p-2" />
+            <button type="submit" className="w-16 rounded-e-md bg-slate-900 text-white hover:bg-slate-800">Add</button>
+        </form>
+    )
+}
